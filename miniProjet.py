6a4e0053790c9fd01688ce91d47437690001cb94
Sigma- <tom_wautelet@hotmail.com>
2015-11-21 14:47:46 +0100

    Ebauche de la fonction display_map()

--- miniProjet.py
+++ miniProjet.py
@@ -1,1 +1,32 @@
+# -*- coding: utf-8 -*-
 # BattleShip Game Python
+
+def display_map(own_map) :
+    """Diplay the Battleship Map
+    Parameters :
+    ------------
+    own_map : your own map given by the file (list)
+    
+    Returns :
+    ---------
+    own_map : your own map (list)
+    
+    """
+    print "  A B C D E F G H I J"
+    for i,j in enumerate(own_map):
+        print i,' '.join(j)
+        
+
+#Exemple pour voir si ça fonctionne
+own_map = [['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.'],
+           ['.', '.', '.', '.','.', '.', '.', '.','.', '.']]
+display_map(own_map)
+        
